2b4deb146f5cab306d67b57378060381480152af
Angelina Sharypina <angelinasharypina2014@gmail.com>
2024-11-19 07:49:03 +0500

    finished version

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import AppContext from "./context";
 
 import Home from "./pages/Home";
 import Favorites from "./pages/Favorites";
+import Orders from "./pages/Orders";
 
 function App() {
   const [items, setItems] = React.useState([])
@@ -18,49 +19,74 @@ function App() {
 
   React.useEffect(() => {
     async function fetchData() {
-      const cartResponse = await axios.get('https://6734ea635995834c8a91588d.mockapi.io/cart')
-      const favoritesResponse = await axios.get('https://6735fbd55995834c8a94e925.mockapi.io/favorites')
-      const itemsResponse = await axios.get('https://6734ea635995834c8a91588d.mockapi.io/items')
+      try {
+        const [cartResponse, favoritesResponse, itemsResponse] = await Promise.all([
+          axios.get('https://6734ea635995834c8a91588d.mockapi.io/cart'), 
+          axios.get('https://6735fbd55995834c8a94e925.mockapi.io/favorites'), 
+          axios.get('https://6734ea635995834c8a91588d.mockapi.io/items')
+        ])
 
-      setIsLoading(false)
-      setCartItems(cartResponse.data)
-      setFavorites(favoritesResponse.data)
-      setItems(itemsResponse.data)
+        setIsLoading(false)
+        setCartItems(cartResponse.data)
+        setFavorites(favoritesResponse.data)
+        setItems(itemsResponse.data)
+      } catch (error) {
+        alert('Ошибка при запросе данных ;(')
+        console.error(error)
+      }
     }
 
     fetchData()
   }, [])
 
-  const onAddToCart = (obj) => {
+  const onAddToCart = async (obj) => {
     try {
-      if (cartItems.find((item) => Number(item.id) === Number(obj.id))) {
-        axios.delete(`https://6734ea635995834c8a91588d.mockapi.io/cart/${obj.id}`)
-        setCartItems(prev => prev.filter(item => Number(item.id) !== Number(obj.id)))
+      const findItem = cartItems.find((item) => Number(item.parentId) === Number(obj.id))
+      if (findItem) {
+        setCartItems(prev => prev.filter(item => Number(item.parentId) !== Number(obj.id)))
+        await axios.delete(`https://6734ea635995834c8a91588d.mockapi.io/cart/${findItem.id}`)
       } else {
-        axios.post('https://6734ea635995834c8a91588d.mockapi.io/cart', obj)
         setCartItems(prev => [...prev, obj])
+        const {data} = await axios.post('https://6734ea635995834c8a91588d.mockapi.io/cart', obj)
+        setCartItems((prev) => prev.map(item => {
+          if (item.parentId === data.parentId) {
+            return {
+              ...item, 
+              id: data.id
+            }
+          }
+          return item
+        }))
       }
     } catch (error) {
-      
+      alert('Ошибка при добавлении в корзину')
+      console.error(error)
     }
   }
 
   const onRemoveItem = (id) => {
-    axios.delete(`https://6734ea635995834c8a91588d.mockapi.io/cart/${id}`)
-    setCartItems(prev => prev.filter(item => item.id !== id))
+    try {
+      axios.delete(`https://6734ea635995834c8a91588d.mockapi.io/cart/${id}`)
+      setCartItems(prev => prev.filter(item => Number(item.id) !== Number(id)))
+    } catch (error) {
+      alert('Ошибка при удалении из корзины')
+      console.error(error)
+    }
   }
 
   const onAddToFavorite = async (obj) => {
+    const findItem = favorites.find((favObj) => Number(favObj.parentId) === Number(obj.id))
     try {
-      if (favorites.find(favObj => Number(favObj.id) === Number(obj.id))) {
-        axios.delete(`https://6735fbd55995834c8a94e925.mockapi.io/favorites/${obj.id}`)
-        setFavorites(prev => prev.filter((item) => Number(item.id) !== Number(obj.id)))
+      if (findItem) {
+        axios.delete(`https://6735fbd55995834c8a94e925.mockapi.io/favorites/${findItem.id}`)
+        setFavorites(prev => prev.filter((item) => Number(item.parentId) !== Number(obj.id)))
       } else {
         const {data} = await axios.post('https://6735fbd55995834c8a94e925.mockapi.io/favorites', obj)
         setFavorites(prev => [...prev, data])
       }
     } catch (error) {
       alert('Не удалось добавить в фавориты')
+      console.error(error)
     }
   }
 
@@ -69,13 +95,22 @@ function App() {
   }
 
   const isItemAdded = (id) => {
-    return cartItems.some(obj => Number(obj.id) === Number(id))
+    return cartItems.some(obj => Number(obj.parentId) === Number(id))
+  }
+
+  const isItemFavorite = (id) => {
+    return favorites.some(obj => Number(obj.parentId) === Number(id))
   }
 
   return (
-    <AppContext.Provider value={{items, cartItems, favorites, isItemAdded, onAddToFavorite, setCartOpened, setCartItems}}>
+    <AppContext.Provider value={{items, cartItems, favorites, isItemAdded, isItemFavorite, onAddToFavorite, onAddToCart, setCartOpened, setCartItems}}>
       <div className="wrapper clear">
-        {cartOpened && <Drawer items={cartItems} onClose={() => setCartOpened(false)} onRemove={onRemoveItem} />}
+        <Drawer 
+          items={cartItems} 
+          onClose={() => setCartOpened(false)} 
+          onRemove={onRemoveItem} 
+          opened={cartOpened} 
+        />
         <Header onClickCart={() => setCartOpened(true)} />
 
         <Routes>
@@ -93,6 +128,9 @@ function App() {
           } />
           <Route path="/favorites" element={
             <Favorites />
+          } />
+           <Route path="/orders" element={
+            <Orders />
           } />
         </Routes>
 
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -15,15 +15,16 @@ function Card({
   favorited = false, 
   loading = false
 }) {
-    const {isItemAdded} = React.useContext(AppContext)
+    const {isItemAdded, isItemFavorite} = React.useContext(AppContext)
     const [isFavorite, setIsFavorite] = React.useState(favorited)
+    const obj = {id, parentId: id, title, imageUrl, price}
 
     const onClickPlus = () => {
-      onPlus({id, title, imageUrl, price})
+      onPlus(obj)
     }
 
     const onClickFavorite = () => {
-      onFavorite({id, title, imageUrl, price})
+      onFavorite(obj)
       setIsFavorite(!isFavorite)
     }
 
@@ -47,9 +48,11 @@ function Card({
             </ContentLoader>
             ) : (
               <>
-                <div className={styles.favorite} onClick={onClickFavorite}>
-                  <img src={isFavorite ? "/img/liked.svg" : "/img/unliked.svg"} alt="Unliked" />
-                </div>
+                {onFavorite && (
+                  <div className={styles.favorite} onClick={onClickFavorite}>
+                    <img src={isItemFavorite(id) || favorited ? "/img/liked.svg" : "/img/unliked.svg"} alt="Unliked" />
+                  </div>
+                )}
                 <img width={133} height={112} src={imageUrl} alt="" />
                 <h5>{title}</h5>
                 <div className="d-flex justify-between align-center">
@@ -57,7 +60,14 @@ function Card({
                     <span>Цена:</span>
                     <b>{price} руб.</b>
                   </div>
-                  <img className={styles.plus} onClick={onClickPlus} src={isItemAdded(id) ? "/img/btn-checked.svg" : "/img/btn-plus.svg"} alt="Plus" />
+                  {onPlus && (
+                    <img 
+                      className={styles.plus} 
+                      onClick={onClickPlus} 
+                      src={isItemAdded(id) ? "/img/btn-checked.svg" : "/img/btn-plus.svg"} 
+                      alt="Plus" 
+                    />
+                  )}
                 </div>
               </>
             )

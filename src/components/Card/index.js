2e23136d0e7f668cb329d84722246779495f6b6f
Angelina Sharypina <angelinasharypina2014@gmail.com>
2024-11-19 08:30:30 +0500

    deploy version

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -50,7 +50,7 @@ function Card({
               <>
                 {onFavorite && (
                   <div className={styles.favorite} onClick={onClickFavorite}>
-                    <img src={isItemFavorite(id) || favorited ? "/img/liked.svg" : "/img/unliked.svg"} alt="Unliked" />
+                    <img src={isItemFavorite(id) || favorited ? "img/liked.svg" : "img/unliked.svg"} alt="Unliked" />
                   </div>
                 )}
                 <img width={133} height={112} src={imageUrl} alt="" />
@@ -64,7 +64,7 @@ function Card({
                     <img 
                       className={styles.plus} 
                       onClick={onClickPlus} 
-                      src={isItemAdded(id) ? "/img/btn-checked.svg" : "/img/btn-plus.svg"} 
+                      src={isItemAdded(id) ? "img/btn-checked.svg" : "img/btn-plus.svg"} 
                       alt="Plus" 
                     />
                   )}
